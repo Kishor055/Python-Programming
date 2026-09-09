2930373c1a7cd7c11dad01a8d400112587621c4e
Kishor Kakde <kishorkakde026@gmail.com>
2026-09-09 23:29:48 +0530

    Update 01_hello_world.py

---
+++ 01-Basics/01_hello_world.py
@@ -0,0 +1,13 @@
+# 01_hello_world.py
+
+# Basic Hello World program
+print("Hello, World!")
+
+# Printing a simple message
+print("Welcome to Python!")
+
+# Printing multiple values
+print("Hello", "Python")
+
+# Printing a number
+print(2026)
