17f56e4bc96e52fbdb66df9a772074b97f55cbe0
Kishor Kakde <kishorkakde026@gmail.com>
2026-09-10 01:24:32 +0530

    Update 04_data_types.py

---
+++ 01-Basics/04_data_types.py
@@ -0,0 +1,37 @@
+# 04_data_types.py
+
+# Integer
+age = 20
+
+# Float
+price = 99.50
+
+# String
+name = "Rahul"
+
+# Boolean
+is_student = True
+
+# List
+fruits = ["Apple", "Banana", "Mango"]
+
+# Tuple
+coordinates = (10, 20)
+
+# Set
+numbers = {1, 2, 3, 4}
+
+# Dictionary
+student = {
+    "name": "Rahul",
+    "age": 20
+}
+
+print("Integer:", age)
+print("Float:", price)
+print("String:", name)
+print("Boolean:", is_student)
+print("List:", fruits)
+print("Tuple:", coordinates)
+print("Set:", numbers)
+print("Dictionary:", student)
