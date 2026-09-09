a7b3dc7dd77e92927874fe36debd53a02460ca77
Kishor Kakde <kishorkakde026@gmail.com>
2026-09-10 01:25:12 +0530

    Add input and output examples in Python

---
+++ 01-Basics/05_input_output.py
@@ -0,0 +1,20 @@
+# 05_input_output.py
+
+# Output
+print("Welcome to Python!")
+
+# Input
+name = input("Enter your name: ")
+
+# Output using input
+print("Hello", name)
+
+# Taking integer input
+age = int(input("Enter your age: "))
+
+print("Your age is:", age)
+
+# Taking float input
+marks = float(input("Enter your marks: "))
+
+print("Your marks are:", marks)
